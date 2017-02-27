c164492dd6661998dc1df0b5cca2ccd6bde6ad83
Arlo Bryan Guthrie <arlo@sproutsocial.com>
2017-02-27 11:35:08 -0600

    Build JSON file of all versions for use in other tools

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -272,12 +272,19 @@ gulp.task('color', [
   'color-clr'
 ]);
 
-gulp.task('docs', () => {
+gulp.task('docs', (cb) => {
   const versionsYaml = Object.keys(versions).map((pkg) => `  ${pkg}: ${versions[pkg]}`).join('\n');
 
   gulp.src('docs/_config.yml')
     .pipe(replace(/(# #versions)[^]+(# \/versions)/gm, '\$1\n' + versionsYaml + '\n\$2'))
     .pipe(gulp.dest('docs'));
+
+  // Write JSON file of versions, excluding the build package
+  fs.writeFile(
+    './docs/downloads/versions.json',
+    JSON.stringify(versions, (key, value) => key === 'seeds' ? undefined : value),
+    (err) => cb(err)
+  );
 });
 
 gulp.task('build', [
