77fedc9b23717c017efecab2f90b5e943504bdf2
Chase McCoy <chasem000@gmail.com>
2017-08-28 10:59:08 -0500

    Add timing values to Motion package

--- packages/seeds-motion/dist/seeds-motion.js
+++ packages/seeds-motion/dist/seeds-motion.js
@@ -3,5 +3,9 @@
 module.exports = {
   MOTION_EASE_IN: 'cubic-bezier(.4, 0, 1, 1)',
   MOTION_EASE_OUT: 'cubic-bezier(0, 0, .2, 1)',
-  MOTION_EASE_INOUT: 'cubic-bezier(.4, 0, .2, 1)'
+  MOTION_EASE_INOUT: 'cubic-bezier(.4, 0, .2, 1)',
+  MOTION_TIMING_IMMEDIATELY: '0.05s',
+  MOTION_TIMING_QUICKLY: '0.1s',
+  MOTION_TIMING_PROMPTLY: '0.2s',
+  MOTION_TIMING_SLOWLY: '0.4s'
 };
