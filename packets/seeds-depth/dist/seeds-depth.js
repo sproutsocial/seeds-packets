4ef5132cf033d63a4c645d1d406a08d193420fb7
Chase McCoy <chasem000@gmail.com>
2018-10-23 13:48:55 -0500

    chore: check in dist files

---
+++ packets/seeds-depth/dist/seeds-depth.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  DEPTH_SHADOW_100: '0 0 3px rgba(0,0,0,.12), 0 1px 2px rgba(0,0,0,.24)',
+  DEPTH_SHADOW_200: '0 0 6px rgba(0,0,0,.16), 0 3px 6px rgba(0,0,0,.23)',
+  DEPTH_SHADOW_300: '0 0 20px rgba(0,0,0,.19), 0 6px 6px rgba(0,0,0,.23)',
+  DEPTH_SHADOW_400: '0 0 28px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.22)',
+  DEPTH_SHADOW_500: '0 0 38px rgba(0,0,0,.3), 0 15px 12px rgba(0,0,0,.22)'
+};
---
+++ packets/seeds-space/dist/seeds-space.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  SPACE_SIZE_0: 0,
+  SPACE_SIZE_100: '2px',
+  SPACE_SIZE_200: '4px',
+  SPACE_SIZE_300: '8px',
+  SPACE_SIZE_350: '12px',
+  SPACE_SIZE_400: '16px',
+  SPACE_SIZE_450: '24px',
+  SPACE_SIZE_500: '32px',
+  SPACE_SIZE_600: '40px'
+};
