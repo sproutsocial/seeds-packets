9625fc1235f7561b0f82565424f24ad9b888ec6d
Chase McCoy <chasem000@gmail.com>
2019-05-17 11:53:34 -0500

    feat(seeds-border): update styles for Nectar
    BREAKING CHANGE: border width 600 has been removed

--- packets/seeds-border/dist/seeds-border.js
+++ packets/seeds-border/dist/seeds-border.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-	BORDER_RADIUS_500: '3px',
-	BORDER_WIDTH_500: '1px',
-	BORDER_WIDTH_600: '2px'
+	BORDER_RADIUS_500: '6px',
+	BORDER_RADIUS_600: '8px',
+	BORDER_RADIUS_1000: '999999px',
+	BORDER_WIDTH_500: '1px'
 };
