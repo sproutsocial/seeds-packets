9942415a1967b0aa4d3e466495a8912cc56bdae7
Chase McCoy <chasem000@gmail.com>
2019-07-12 12:06:43 -0500

    fix(seeds-depth): fix 400 token value

--- packets/seeds-depth/dist/seeds-depth.js
+++ packets/seeds-depth/dist/seeds-depth.js
@@ -4,5 +4,5 @@ module.exports = {
 	ELEVATION_LEVEL_100: '0px 2px 4px rgba(39,51,51,.24)',
 	ELEVATION_LEVEL_200: '0px 4px 8px rgba(39,51,51,.24)',
 	ELEVATION_LEVEL_300: '0px 8px 16px rgba(39,51,51,.24)',
-	ELEVATION_LEVEL_400: '0px 16px 32px rgba(30,51,51,.24)'
+	ELEVATION_LEVEL_400: '0px 16px 32px rgba(39,51,51,.24)'
 };
