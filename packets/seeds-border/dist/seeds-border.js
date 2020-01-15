40e7dfe7db7ed3576d99646939f0d814fd1ce6da
Justin Conway <justin@conwaydev.com>
2020-01-15 12:12:50 -0600

    fix: fix border radius and width regressions

--- packets/seeds-border/dist/seeds-border.js
+++ packets/seeds-border/dist/seeds-border.js
@@ -1,8 +1,10 @@
 'use strict';
 
 module.exports = {
+	BORDER_RADIUS_400: '3px',
 	BORDER_RADIUS_500: '6px',
 	BORDER_RADIUS_600: '8px',
 	BORDER_RADIUS_1000: '999999px',
-	BORDER_WIDTH_500: '1px'
+	BORDER_WIDTH_500: '1px',
+	BORDER_WIDTH_600: '2px'
 };
