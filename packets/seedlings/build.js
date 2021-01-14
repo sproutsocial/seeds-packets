984d54cd8911ee168338b245cdb25dc95fd2eed9
Justin Conway <justin@conwaydev.com>
2021-01-13 22:59:07 -0600

    chore: update js fix

--- packets/seedlings/build.js
+++ packets/seedlings/build.js
@@ -20,23 +20,23 @@ https://github.com/sproutsocial/seeds-packets/tree/main/packets/seedlings
 const source = './seedlings-marketing.scss';
 
 sass.render(
-    {
-      file: source,
-      outFile: `./dist/${source}`,
-      includePaths: ['node_modules']
-    },
-    (err, result) => {
+  {
+    file: source,
+    outFile: `./dist/${source}`,
+    includePaths: ['node_modules']
+  },
+  (err, result) => {
+    if (err) {
+      throw err;
+    }
+
+    const data = `${BANNER} \n ${result.css}`;
+    fsextra.outputFile(`./dist/${source.replace('scss', 'css')}`, data, err => {
       if (err) {
         throw err;
+      } else {
+        console.info(`✔︎ dist/${source.replace('./', '').replace('scss', 'css')}`);
       }
-
-      const data = `${BANNER} \n ${result.css}`;
-      fsextra.outputFile(`./dist/${source.replace('scss', 'css')}`, data, err => {
-        if (err) {
-          throw err;
-        } else {
-          console.info(`✔︎ dist/${source.replace('./', '').replace('scss', 'css')}`);
-        }
-      });
-    }
+    });
+  }
 );
